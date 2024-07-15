c411cd70e082b7dab47073f0095ad42f144b1ea9
HayKor <baginian.artur@yandex.ru>
2024-07-15 12:45:35 +0300

    some tests

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ import os
 load_dotenv()
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
+FATHER_CHAT_ID = os.environ.get("FATHER_CHAT_ID")
--- main.py
+++ main.py
@@ -4,17 +4,41 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from config import BOT_TOKEN
+from config import BOT_TOKEN, FATHER_CHAT_ID
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
-@dp.message()
-async def send_pvz_poll(message: types.Message):
-    await message.reply(
-        text=message.text,
+# @dp.message()
+# async def send_info(message: types.Message):
+#     await bot.send_message(
+#         chat_id=FATHER_CHAT_ID,
+#         text=message.text,
+#     )
+#
+
+
+async def send_to_father(bot: Bot):
+    await bot.send_message(
+        chat_id=FATHER_CHAT_ID,
+        text="test every 5 sec",
     )
+
+
+async def send_attendance_poll(bot: Bot):
+    await bot.send_poll(
+        chat_id=FATHER_CHAT_ID,
+        question="What do you choose?",
+        options=["A", "B", "C"],
+        type="regular",
+        is_anonymous=False,
+    )
+
+
+def set_scheduled_jobs(scheduler: AsyncIOScheduler, bot: Bot, *args, **kwargs):
+    # scheduler.add_job(send_to_father, "interval", seconds=5, args=(bot,))
+    scheduler.add_job(send_attendance_poll, "interval", seconds=5, args=(bot,))
 
 
 async def main():
@@ -22,10 +46,12 @@ async def main():
         level=logging.INFO,
     )
     sched = AsyncIOScheduler()
+    set_scheduled_jobs(scheduler=sched, bot=bot)
 
     try:
-        await dp.start_polling(bot)
         sched.start()
+        await dp.start_polling(bot)
+
     finally:
         await dp.storage.close()
         await bot.session.close()
