f8ac88210e0ab1767adba6691355717a80d6d415
HayKor <baginian.artur@yandex.ru>
2024-07-21 15:00:58 +0300

    some debugs

--- pvzbot/database/engine.py
+++ pvzbot/database/engine.py
@@ -1,9 +1,5 @@
 from config import config
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine,
-)
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 
 engine = create_async_engine(
--- pvzbot/filters.py
+++ pvzbot/filters.py
@@ -1,11 +1,9 @@
 from aiogram import types
 from aiogram.filters import Filter
-from database.database import get_user_by_id
 from database.engine import async_session
-from database.models import Users
+from database.user_crud import get_user_by_id
 
 
-# TODO
 class IsFather(Filter):
     async def __call__(self, message: types.Message) -> bool:
         user = await get_user_by_id(async_session, message.from_user.id)
