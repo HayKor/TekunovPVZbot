a676de17db3c324e7822191b1ad58b12e362b98d
Arthur <baginian.artur@yandex.com>
2024-08-20 18:23:37 +0300

    Useless mapped_collumn() deleted from models' fields

--- pvzbot/database/models.py
+++ pvzbot/database/models.py
@@ -7,38 +7,37 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(AsyncAttrs, DeclarativeBase):
     id: Mapped[int] = mapped_column(
-        "id", Integer(), primary_key=True, autoincrement=True
+        Integer(), primary_key=True, autoincrement=True
     )
 
 
 class Users(Base):
     __tablename__ = "users"
-    nickname: Mapped[str] = mapped_column("nickname", nullable=False)
-    is_admin: Mapped[bool] = mapped_column("is_admin", default=False)
-    is_father: Mapped[bool] = mapped_column("is_father", default=False)
+    nickname: Mapped[str]
+    is_admin: Mapped[bool] = mapped_column(default=False)
+    is_father: Mapped[bool] = mapped_column(default=False)
 
 
 class Points(Base):
     __tablename__ = "points"
-    address: Mapped[str] = mapped_column("address", nullable=False)
-    type: Mapped[str] = mapped_column("type", nullable=False)
+    address: Mapped[str]
+    type: Mapped[str]
 
 
 class Office(Base):
     __tablename__ = "office"
-    occupation: Mapped[str] = mapped_column("occupation", nullable=False)
-    name: Mapped[str] = mapped_column("name", nullable=False)
-    tg_nickname: Mapped[str] = mapped_column("tg_nickname", nullable=True)
-    phone: Mapped[str] = mapped_column("phone", nullable=True)
-    schedule: Mapped[str] = mapped_column("schedule", nullable=True)
-    description: Mapped[str] = mapped_column("description", nullable=True)
+    occupation: Mapped[str]
+    name: Mapped[str]
+
+    tg_nickname: Mapped[str | None]
+    phone: Mapped[str | None]
+    schedule: Mapped[str | None]
+    description: Mapped[str | None]
 
 
 class Revisions(Base):
     __tablename__ = "revisions"
-    date: Mapped[dat] = mapped_column(
-        "date", Date(), default=func.current_date()
-    )
+    date: Mapped[dat] = mapped_column(Date(), default=func.current_date())
 
     polls: Mapped[list["Polls"]] = relationship(
         "Polls", back_populates="revision", lazy="selectin"
@@ -48,7 +47,7 @@ class Revisions(Base):
 class Polls(Base):
     __tablename__ = "polls"
     revision_id: Mapped[int] = mapped_column(
-        "revision_id", ForeignKey("revisions.id"), default=-1
+        ForeignKey("revisions.id"), default=-1
     )
 
     poll_answers: Mapped[list["PollAnswers"]] = relationship(
@@ -61,10 +60,10 @@ class Polls(Base):
 
 class PollAnswers(Base):
     __tablename__ = "poll_answers"
-    poll_id: Mapped[int] = mapped_column("poll_id", ForeignKey("polls.id"))
-    question: Mapped[str] = mapped_column("question", nullable=False)
-    option_id: Mapped[int] = mapped_column("option_id", nullable=False)
-    is_answered: Mapped[bool] = mapped_column("is_answered", default=False)
+    poll_id: Mapped[int] = mapped_column(ForeignKey("polls.id"))
+    question: Mapped[str]
+    option_id: Mapped[int]
+    is_answered: Mapped[bool] = mapped_column(default=False)
 
     poll: Mapped["Polls"] = relationship(
         "Polls", back_populates="poll_answers", lazy="selectin"
